6d775683fe73284673fea6c659168f4fe9c847b2
mkzxc <mirkozxc@gmail.com>
2026-04-10 13:00:53 +0200

    feat(entities): onError async typing

--- src/library/entities/action.ts
+++ src/library/entities/action.ts
@@ -2,7 +2,7 @@ interface Action<T, K, Z> {
   key: T;
   fetchFn: (payload: K) => Z;
   onSuccess?: (data: Z) => void | Promise<void>;
-  onError?: (error: Error) => void;
+  onError?: (error: Error) => void | Promise<void>;
 }
 
 export type { Action };
